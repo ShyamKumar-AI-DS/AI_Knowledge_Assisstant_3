c122b00246915645f06bba219fda8226337874d2
Santino_44 <93427182+ShyamKumar-AI-DS@users.noreply.github.com>
2025-10-30 13:25:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 # LangChain
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA, LLMChain
